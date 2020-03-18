e0102bf736fd6cf0f0ace0baceeafcc82db8fa06
Parthiv Mohan <parthiv.mohan@gmail.com>
2020-03-18 14:58:12 -0400

    testing

--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -1,17 +1,19 @@
-import { mount, shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import App from './../../src/App.vue'
-import Calculator from './../../src/components/Calculator.vue'
 
 describe('App', () => {
-	const wrapper = shallowMount(App)
+	// Inspect the raw component options
+	it('has data', () => {
+		expect(typeof App.data).toBe('function')
+	})
+})
+
+describe('Mounted App', () => {
+	const wrapper = mount(App);
 
 	test('is a Vue instance', () => {
 		expect(wrapper.isVueInstance()).toBeTruthy()
-	})	
-})
-
-describe('App and Calculator', () => {
-	const wrapper = mount(App)
+	})
 
 	it('renders the correct markup', () => {
 	  expect(wrapper.html()).toContain('<p>What is the sum of the two numbers?</p>')
@@ -22,50 +24,41 @@ describe('App and Calculator', () => {
 	  expect(wrapper.contains('button')).toBe(true)
 	})
 
-	// Inspect the raw component options
-	it('has a data hook', () => {
-		expect(typeof wrapper.find(Calculator).vm.$methods).toBe('function')
-	})
-
-	it('renders correctly with different props', () => {
-		expect(
-			getMountedComponent(MyComponent, {
-				msg: 'Hello'
-			}).text()
-		).toBe('Hello')
-
-		expect(
-			getMountedComponent(MyComponent, {
-				msg: 'Bye'
-			}).text()
-		).toBe('Bye')
-	})
-
-	// Inspect the generated HTML after a state update
-	it('updates the rendered message when wrapper.message updates', async () => {
-		const wrapper = shallowMount(MyComponent)
-		wrapper.setData({ message: 'foo' })
-
-		// Wait a "tick" after state change before asserting DOM updates
+	it('renders correctly with different data', async () => {
+		wrapper.setData({ x1: 5, x2: 10 })
 		await wrapper.vm.$nextTick()
-		expect(wrapper.text()).toBe('foo')
+		expect(wrapper.text()).toContain('10')
 	})
 
-	// Evaluate the results of functions in
-	// the raw component options
-	it('sets the correct default data', () => {
-		expect(typeof MyComponent.data).toBe('function')
-		const defaultData = MyComponent.data()
-		expect(defaultData.message).toBe('hello!')
+	it('button click without correct sum', () => {
+	  expect(wrapper.vm.message).toBe("")
+	  const button = wrapper.find('button')
+	  button.trigger('click')
+	  expect(wrapper.vm.message).toBe('TRY AGAIN')
 	})
 
-	// Inspect the component instance on mount
-	it('correctly sets the message when created', () => {
-		expect(wrapper.vm.$data.message).toBe('bye!')
+	it('button click with correct sum', () => {
+	  wrapper.setData({ guess: "15" })
+	  const button = wrapper.find('button')
+	  button.trigger('click')
+	  expect(wrapper.vm.message).toBe('SUCCESS!')
 	})
 
-	// Mount an instance and inspect the render output
-	it('renders the correct message', () => {
-		expect(wrapper.text()).toBe('bye!')
-	})
+	// // Evaluate the results of functions in
+	// // the raw component options
+	// it('sets the correct default data', () => {
+	// 	expect(typeof MyComponent.data).toBe('function')
+	// 	const defaultData = MyComponent.data()
+	// 	expect(defaultData.message).toBe('hello!')
+	// })
+
+	// // Inspect the component instance on mount
+	// it('correctly sets the message when created', () => {
+	// 	expect(wrapper.vm.$data.message).toBe('bye!')
+	// })
+
+	// // Mount an instance and inspect the render output
+	// it('renders the correct message', () => {
+	// 	expect(wrapper.text()).toBe('bye!')
+	// })
 })
